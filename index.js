d65ad237a564abbdd9a64883ed1ac63d90531b11
cpryan3 <cpryan3@rams.colostate.edu>
2020-11-03 11:48:35 -0700

    11.1 11/3 POST /reviewer/login

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const db = require('./dbConnectExec.js')
 
 const bcrypt = require('bcryptjs')
 
+const jwt = require('jsonwebtoken')
+
+const config = require('./config.js')
+
 const app = express();
 app.use(express.json())
 
@@ -14,6 +18,87 @@ app.get("/hi",(req,res)=>{
 // app.post()
 // app.put()
 // app.delete()
+
+app.post("/reviewer/login", async (req,res)=>{
+    // console.log('/reviewer/login called')
+    console.log(req.body)
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    if(!email || !password){
+        return res.status(400).send('bad request')
+    }
+
+    //1. check that user email exsists in db
+    var query = `Select *
+    FROM reviewer
+    where email = '${email}'`
+
+    // var result = await db.executeQuery(query);
+
+    let result;
+
+    try{
+        result = await db.executeQuery(query);
+    }catch(myError){
+        console.log('error in reviewer/login', myError);
+        return res.status(500).send()
+    }
+
+    // console.log(result)
+
+    if(!result[0]){
+        return res.status(400).send('Invalid user credentials')
+    }
+
+    //2. check their password
+
+    let user = result[0]
+    // console.log(user)
+    // this if statement works but i think i can't use it beacuse of the hash think
+    // if(!bcrypt.compareSync(password,user.password)){
+    //     console.log("invalid password")
+    //     return res.status(400).send("invalid user crendentials")
+    // }
+    //This is just a place holder until i can use the hashed password
+    if(user.password != password){
+        console.log("invalid password")
+        return res.status(400).send("invalid user crendentials")
+    }
+
+
+    //3. generate a token
+
+    let token = jwt.sign({pk: user.ReviwerPK}, config.JWT, {expiresIn: '60 minutes'})
+
+    // console.log(token)
+
+    //4. save the token in db and send token and user info
+
+    let setTokenQuery = `UPDATE Reviewer
+    SET Ticket = '${token}'
+    WHERE ReviewerPK = ${user.ReviewerPK}`
+
+    try{
+        await db.executeQuery(setTokenQuery)
+
+        res.status(200).send({
+            ticket: token,
+            user: {
+                FName: user.FName,
+                LName: user.LName,
+                Email: user.Email,
+                ReviewerPK: user.ReviewerPK
+            }
+        })
+    }
+    catch(myError){
+        console.log("error setting user token ", myError)
+        res.status(500).send()
+    }
+})
+
 app.post("/reviewer", async (req,res)=> {
     // res.send("creating user")
     // console.log("request body", req.body)
